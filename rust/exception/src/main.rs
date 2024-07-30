c01cd928658f4d0d898fefd54894c8b8b88cbfae
Yuekai Jia <equation618@gmail.com>
2024-07-30 21:15:09 +0800

    Add page fault test in rust/exception

--- rust/exception/src/main.rs
+++ rust/exception/src/main.rs
@@ -16,11 +16,34 @@ fn raise_break_exception() {
         #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
         asm!("ebreak");
     }
+    println!("Breakpoint test OK!");
+}
+
+#[cfg(feature = "axstd")]
+fn raise_page_fault() {
+    use axhal::{mem::VirtAddr, paging::MappingFlags};
+    use std::os::arceos::modules::axhal;
+
+    #[linkme::distributed_slice(axhal::trap::PAGE_FAULT)]
+    fn page_fault_handler(vaddr: VirtAddr, access_flags: MappingFlags, is_user: bool) -> bool {
+        println!(
+            "Page fault @ {:#x}, access_flags: {:?}, is_user: {}",
+            vaddr, access_flags, is_user
+        );
+        println!("Page fault test OK!");
+        axhal::misc::terminate();
+    }
+
+    let fault_addr = 0xdeadbeef as *mut u8;
+    unsafe {
+        *(fault_addr) = 233;
+    }
 }
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     println!("Running exception tests...");
     raise_break_exception();
-    println!("Exception tests run OK!");
+    #[cfg(feature = "axstd")]
+    raise_page_fault();
 }
--- rust/net/bwbench/src/main.rs
+++ rust/net/bwbench/src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 
-extern crate axstd;
+extern crate axstd as std;
+
+use std::os::arceos::modules::axnet;
 
 #[no_mangle]
 fn main() {
