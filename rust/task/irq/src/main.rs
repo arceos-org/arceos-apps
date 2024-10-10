82cde311fe4f69e99959f188daad6f485bdda855
hky1999 <976929993@qq.com>
2024-10-11 00:08:16 +0800

    Add task irq state test for sleep and wait

--- rust/task/irq/src/main.rs
+++ rust/task/irq/src/main.rs
@@ -8,78 +8,130 @@ extern crate axstd as std;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 
-#[cfg(feature = "axstd")]
-use std::os::arceos::modules::axhal;
-
-const NUM_TASKS: usize = 10;
+const NUM_TASKS: usize = 16;
 const NUM_TIMES: usize = 100;
-static FINISHED_TASKS: AtomicUsize = AtomicUsize::new(0);
 
-fn assert_irq_enabled() {
-    #[cfg(feature = "axstd")]
-    {
-        assert!(
-            axhal::arch::irqs_enabled(),
-            "Task id = {:?} IRQs should be enabled!",
-            thread::current().id()
-        );
+mod irq;
+use irq::*;
+
+fn test_yielding() {
+    println!("Hello, main task test_yielding()!");
+    static YIELDING_FINISHED_TASKS: AtomicUsize = AtomicUsize::new(0);
+    for _i in 0..NUM_TASKS {
+        thread::spawn(move || {
+            assert_irq_enabled();
+            for _t in 0..NUM_TIMES {
+                assert_irq_enabled();
+                thread::yield_now();
+                assert_irq_enabled_and_disabled();
+            }
+
+            let _ = YIELDING_FINISHED_TASKS.fetch_add(1, Ordering::Relaxed);
+        });
     }
-}
 
-fn assert_irq_disabled() {
-    #[cfg(feature = "axstd")]
-    {
-        assert!(
-            !axhal::arch::irqs_enabled(),
-            "Task id = {:?} IRQs should be disabled!",
-            thread::current().id()
-        );
+    while YIELDING_FINISHED_TASKS.load(Ordering::Relaxed) < NUM_TASKS {
+        thread::yield_now();
+        assert_irq_enabled_and_disabled();
     }
+
+    println!("IRQ state tests on task yield run OK!");
 }
 
-fn disable_irqs() {
-    #[cfg(feature = "axstd")]
-    axhal::arch::disable_irqs()
+fn test_sleep() {
+    use std::time::Duration;
+
+    static SLEEP_FINISHED_TASKS: AtomicUsize = AtomicUsize::new(0);
+
+    println!("Hello, main task test_sleep()!");
+
+    assert_irq_enabled();
+    thread::sleep(Duration::from_secs(1));
+    assert_irq_enabled_and_disabled();
+
+    // backgroud ticks, 0.1s x 10 = 1s
+    thread::spawn(|| {
+        for _i in 0..10 {
+            assert_irq_enabled();
+            thread::sleep(Duration::from_millis(100));
+            assert_irq_enabled_and_disabled();
+        }
+    });
+
+    // task n: sleep 3 x 1 (sec)
+    for _i in 0..NUM_TASKS {
+        thread::spawn(move || {
+            assert_irq_enabled();
+            let sec = 1;
+            for _j in 0..3 {
+                thread::sleep(Duration::from_secs(sec as _));
+                assert_irq_enabled_and_disabled();
+            }
+            SLEEP_FINISHED_TASKS.fetch_add(1, Ordering::Relaxed);
+        });
+    }
+
+    while SLEEP_FINISHED_TASKS.load(Ordering::Relaxed) < NUM_TASKS {
+        thread::sleep(Duration::from_millis(10));
+    }
+    println!("IRQ state tests on task sleep run OK!");
 }
 
-fn enable_irqs() {
-    #[cfg(feature = "axstd")]
-    axhal::arch::enable_irqs()
+#[cfg(feature = "axstd")]
+fn test_wait_queue() {
+    use std::os::arceos::modules::axtask;
+
+    use axtask::WaitQueue;
+
+    static WQ1: WaitQueue = WaitQueue::new();
+    static WQ2: WaitQueue = WaitQueue::new();
+    static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+    println!("Hello, main task test_wait_queue()!");
+
+    for _ in 0..NUM_TASKS {
+        axtask::spawn(move || {
+            assert_irq_enabled();
+
+            // equals to sleep(100ms)
+            WQ1.wait_timeout_until(std::time::Duration::from_millis(100), || false);
+            assert_irq_enabled_and_disabled();
+
+            COUNTER.fetch_add(1, Ordering::Relaxed);
+            WQ1.notify_one(true); // WQ1.wait_until()
+            assert_irq_enabled();
+            WQ2.wait();
+
+            assert_irq_enabled_and_disabled();
+
+            COUNTER.fetch_sub(1, Ordering::Relaxed);
+            WQ1.notify_one(true); // WQ1.wait_until()
+        });
+    }
+    assert_irq_enabled();
+
+    WQ1.wait_until(|| COUNTER.load(Ordering::Relaxed) == NUM_TASKS);
+
+    assert_irq_enabled_and_disabled();
+
+    assert_eq!(COUNTER.load(Ordering::Relaxed), NUM_TASKS);
+    WQ2.notify_all(true); // WQ2.wait()
+
+    assert_irq_enabled();
+    WQ1.wait_until(|| COUNTER.load(Ordering::Relaxed) == 0);
+    assert_irq_enabled_and_disabled();
+    assert_eq!(COUNTER.load(Ordering::Relaxed), 0);
+
+    println!("IRQ state tests on task wait run OK!");
 }
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    for i in 0..NUM_TASKS {
-        thread::spawn(move || {
-            println!("Hello, task {}! id = {:?}", i, thread::current().id());
+    println!("Hello, main task");
 
-            for _t in 0..NUM_TIMES {
-                assert_irq_enabled();
-                thread::yield_now();
+    test_yielding();
+    test_sleep();
+    test_wait_queue();
 
-                disable_irqs();
-                assert_irq_disabled();
-                enable_irqs();
-            }
-
-            println!(
-                "Task {}! id = {:?} irq state test ok",
-                i,
-                thread::current().id()
-            );
-
-            let _ = FINISHED_TASKS.fetch_add(1, Ordering::Relaxed);
-        });
-    }
-
-    println!("Hello, main task, id = {:?}", thread::current().id());
-    while FINISHED_TASKS.load(Ordering::Relaxed) < NUM_TASKS {
-        assert_irq_enabled();
-        thread::yield_now();
-
-        disable_irqs();
-        assert_irq_disabled();
-        enable_irqs();
-    }
     println!("Task irq state tests run OK!");
 }
