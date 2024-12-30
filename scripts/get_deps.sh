53914e4b79b3f69d19e42d5283639e72aa6bab1f
Yuekai Jia <equation618@gmail.com>
2024-12-30 13:16:02 +0800

    Support ArceOS new config system (#5)
    * Add make defconfig & oldconfig, cache AX_ROOT

--- scripts/get_deps.sh
+++ scripts/get_deps.sh
@@ -5,7 +5,4 @@ AX_ROOT=.arceos
 test ! -d "$AX_ROOT" && echo "Cloning repositories ..." || true
 test ! -d "$AX_ROOT" && git clone https://github.com/arceos-org/arceos "$AX_ROOT" --depth=1 || true
 
-echo "Copying Cargo.lock ..."
-cp "$AX_ROOT/Cargo.lock" Cargo.lock
-
 $(dirname $0)/set_ax_root.sh $AX_ROOT
